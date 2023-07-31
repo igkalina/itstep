ac1f352179cce6fa8a9d6b8d4727b1c1ebebd4c5
igkalina <o668105769@gmail.com>
2023-07-31 14:13:46 +0300

    pastrami

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('test')
+sandwich_orders = ['pastrami','pastrami','pastrami','ketchup','mustard']
+sandwich_finiched = []
+print('pastrami is not')
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+sandwich_finiched = sandwich_orders[:]
+print(sandwich_finiched)
